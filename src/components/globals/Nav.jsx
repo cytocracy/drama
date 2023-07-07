91384dab28ffd01624033848095536450e8185e1
cytocracy <theop.parker@gmail.com>
2023-07-06 19:35:18 -0700

    insta

--- src/components/globals/Nav.jsx
+++ src/components/globals/Nav.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../imgs/logo.png'
 import './Nav.css'
+//import fontawesome instagram icon
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 class Nav extends React.Component {
     constructor(props) {
@@ -9,14 +12,15 @@ class Nav extends React.Component {
         super(props);
         this.state = {
             scrollPositionY: 0,
-            class: 'nav'
+            class: 'nav',
+            class2: 'site-name'
         }
     }
     listenScrollEvent = e => {
         if (window.scrollY > 50){
-            this.setState({scrollPositionY: window.scrollY, class: 'nav-scroll'})
+            this.setState({scrollPositionY: window.scrollY, class: 'nav-scroll', class2: 'site-name-scroll'})
         } else {
-            this.setState({scrollPositionY: window.scrollY, class: 'nav'})
+            this.setState({scrollPositionY: window.scrollY, class: 'nav', class2: 'site-name'})
         }
     }
     componentDidMount() {
@@ -27,19 +31,25 @@ class Nav extends React.Component {
             <div className={this.state.class}>
 
                 <div className='logo'>
-                    <Link to="/">
+                    <Link to="/" className='flex items-center'>
                        {/* image from mgs folder */}
                           <img src={logo} alt="logo" className=' w-10' />
-
+                            <p className={this.state.class2}>SHP Drama</p>
                     </Link>
+                    
                 </div>
                 
-                <div className='justify-self-end list-none flex w-96 justify-between items-center pr-5'>
+                <div className='justify-self-end list-none flex w-[27rem] justify-between items-center pr-5'>
                     
                         <Link to="/" >Home</Link>
                         <Link to="/events">Events</Link>
                         <Link to="/audition">Audition Info</Link>
-                        <Link to="/social">Socials</Link>
+                        {/* <Link to="/social">Socials</Link>  */}
+
+                        <a href='https://www.instagram.com/shp.drama/' className='flex items-center'>
+                            {/* add fontawesome instagram icon */}
+                            <FontAwesomeIcon icon={faInstagram} className=' h-6'/>
+                        </a>
                     
                         {/* <Link to="/test" >Test</Link>
 
