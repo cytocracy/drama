e954b4caf053f3c5d7c3b38508f8b28077e8012d
cytocracy <theop.parker@gmail.com>
2023-07-07 19:40:39 -0700

    not found page

--- src/components/globals/NotFound.jsx
+++ src/components/globals/NotFound.jsx
@@ -2,14 +2,19 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
+import meme1 from '../../imgs/IMG_5106.JPG'
+import meme2 from '../../imgs/IMG_1956.JPEG'
 
 class NotFound extends React.Component {
 
     render() {
         return (
-            <div>
-                <h1>Not Found</h1>
-                <Link to="/">Go Home</Link>
+            <div className='pt-[4.5rem]'>
+                <h1>Uh Oh! This page doesn't exist. Enjoy some memes for your troubles.</h1>
+                <div className='flex justify-center'>   
+                    <img src={meme1} alt='meme' className=' h-96' />
+                    <img src={meme2} alt='meme' className='h-96'/>
+                </div>
             </div>
         );
     }
